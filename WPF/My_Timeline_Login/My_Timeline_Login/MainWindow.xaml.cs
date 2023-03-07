e8dcc52abaf851b199736a2ab85b5c7f27a9e3fa
AnxoGomez <angomez3600@gmail.com>
2023-03-07 11:42:15 +0100

    avance login

--- WPF/My_Timeline_Login/My_Timeline_Login/MainWindow.xaml.cs
+++ WPF/My_Timeline_Login/My_Timeline_Login/MainWindow.xaml.cs
@@ -15,14 +15,68 @@ using System.Windows.Shapes;
 
 namespace My_Timeline_Login
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
+  
+    
     public partial class MainWindow : Window
     {
+        //true = register  false = login
+        bool mode;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            mode = true;
+            changeMode(mode);
+        }
+
+        //clickar los botones
+        void changeMode(bool n)
+        {
+            if (n)
+            {
+                var bc = new BrushConverter();
+                registerColor.Background = (Brush)bc.ConvertFrom("#ff3f79");
+                loginColor.Background = Brushes.Transparent;
+            }
+            else
+            {
+                var bc = new BrushConverter();
+                loginColor.Background = (Brush)bc.ConvertFrom("#ff3f79");
+                registerColor.Background = Brushes.Transparent;
+            }
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            mode = true;
+            changeMode(mode);
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            mode = false;
+            changeMode(mode);
+        }
+
+        private void usernameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void usernameBox_Enter(object sender, RoutedEventArgs e)
+        {
+            if(usernameBox.Text.Equals("Usuario"))
+            usernameBox.Text = "";
+        }
+
+        private void usernameBox_Out(object sender, RoutedEventArgs e)
+        {
+            if (usernameBox.Text.Equals(""))
+            {
+                usernameBox.Text = "Usuario";
+            }
         }
     }
 }
